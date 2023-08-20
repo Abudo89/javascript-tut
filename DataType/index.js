b46e3870930fe781a5f82d927afeed5e7bedb9c8
Abudo89 <dubaabdi@gmail.com>
2023-08-20 11:09:34 +0300

    Add files via upload
    Datatypes are basically type of data that can be used and manipulated in a program

---
+++ DataType/index.js
@@ -0,0 +1,21 @@
+let name = "Abudo"; //String data type
+let age = 30; // Number
+let isApproved = false; // Boolean
+let firstName = undefined;
+let selectedColor = null;
+/* JavaScript supports 3 primitive datatypes:
+-Numbers
+-String
+-Boolean
+Except for the above 3 primitive data types, JavaScript also supports two trivial datatypes
+-null
+-undefined
+and one composite datatype 
+-object
+*/
+
+//JavaScript objects are written with curly braces.
+//object properties are written as name: value pairs, separated by commas.
+//The object (person) in the eg below has 4 properties:firstName, lastName, age & eyeColor.
+var person = {firstName:"Abudo", lastName:"Duba", age:30, eyeColor:"black"};
+console.log(person.firstName + "is" + person.age + "years old.");
