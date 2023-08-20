fd1480484cb54225dd9f6b8ffbeb67d94c82485e
Abudo89 <dubaabdi@gmail.com>
2023-08-20 06:35:09 +0300

    Add files via upload
    Tip 1: Use console.table() to Display Arrays and Objects in the Console

---
+++ ArrObj/index.js
@@ -0,0 +1,8 @@
+//Use console.table() to Display Arrays and Objects in the Console
+/* console.log will display the array or object as usual, but console.
+table will tabularize the index and values for you.*/
+
+const myArr = ['Kolade', 'Chelsea', 10, true];
+
+console.log(myArr);
+console.table(myArr);
---
+++ ArrObj/index2.js
@@ -0,0 +1,9 @@
+//It works with objects the same way it works with arrays:
+const myObj = {
+    name: 'Kolade',
+    luckyNum: 10,
+    lovesFootball: true,
+  };
+  
+  console.log(myObj);
+  console.table(myObj);
